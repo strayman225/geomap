d0b0ce4ac4441c44b5e6b8f4998da0d55153c9c4
strayman225 <67448975+strayman225@users.noreply.github.com>
2024-01-22 13:47:33 +0800

    Update settings.py
    as

--- geomap/settings.py
+++ geomap/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "marker/static")
+    os.path.join(BASE_DIR, "static")
 ]
 
 
